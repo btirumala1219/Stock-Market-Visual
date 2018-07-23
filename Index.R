1ff9534fc008e397061e1d6deb9d7c0d15d1aa55
btirumala <barath.tirumala@gmail.com>
2018-07-22 23:10:34 -0400

    graph apple stock

---
+++ Index.R
@@ -0,0 +1,11 @@
+# Get quantmod
+if (!require("quantmod")) {
+    install.packages("quantmod")
+    library(quantmod)
+}
+
+start <- as.Date("2017-01-01")
+end <- as.Date("2017-10-01")
+
+getSymbols("AAPL", src = "yahoo", from = start, to = end)
+plot(AAPL[, "AAPL.Close"], main = "AAPL")
